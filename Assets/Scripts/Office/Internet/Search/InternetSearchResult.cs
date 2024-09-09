d396f120d3262f40d41292b70cbcf5addc72fab4
Nytrock <savvazhirnov@gmail.com>
2024-09-09 19:19:29 +0300

    Add jokes to Internet search

--- Assets/Scripts/Office/Internet/Search/InternetSearchResult.cs
+++ Assets/Scripts/Office/Internet/Search/InternetSearchResult.cs
@@ -10,7 +10,7 @@ public class InternetSearchResult : MonoBehaviour {
     public bool QueryContainKeywords(string query) {
         query = query.ToLower();
         foreach (var keyword in _keywords)
-            if (query.Contains(keyword))
+            if (query == keyword)
                 return true;
         return false;
     }
