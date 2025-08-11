ef9d4bbbef69fab6ca469424d7c00e51cb4266de
Nytrock <savvazhirnov@gmail.com>
2025-08-11 22:02:16 +0300

    Fix not working blur

--- Assets/_Project/Scripts/General/Managers/Fatigue/Tired/CameraTiredAnimator.cs
+++ Assets/_Project/Scripts/General/Managers/Fatigue/Tired/CameraTiredAnimator.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
-[RequireComponent(typeof(Volume))]
+[RequireComponent(typeof(PostProcessVolume))]
 public class CameraTiredAnimator : MonoBehaviour {
     [SerializeField] private RectTransform _panel;
     [SerializeField] private RangeFloat _panelPosition;
     [SerializeField] private RangeFloat _blurCoef;
 
-    private BlurVolumeComponent _depth;
+    private DepthOfField _depth;
 
     private void Awake() {
-        GetComponent<Volume>().profile.TryGet(out _depth);
+        GetComponent<PostProcessVolume>().profile.TryGetSettings(out _depth);
     }
 
     private void Update() {
         float animationCoef = 1 - _panelPosition.InverseLerp(_panel.anchoredPosition.y);
-        _depth.VerticalBlur.value = _blurCoef.Lerp(animationCoef);
+        _depth.focalLength.value = _blurCoef.Lerp(animationCoef);
     }
 }
